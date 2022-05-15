6a555236717c4ebe0205dc74158f6371a0ffc4b3
Adam Azad <adam@adamazad.com>
2022-05-15 10:33:44 +0100

    feat(#22): redesign ENS name card to cap long domains

--- src/components/EnsNameCard/styleds.ts
+++ src/components/EnsNameCard/styleds.ts
@@ -2,45 +2,47 @@ import styled from 'styled-components';
 import { Flex } from 'rebass';
 
 import { ExternalLink, NimiSignatureColor } from '../../theme';
-import { Button } from '../Button';
 
-export const CardWrapper = styled(Flex)`
+export const StyledENSNameCardWrapper = styled(Flex)`
+  justify-content: center;
   align-items: center;
   background: rgba(255, 255, 255, 0.6);
   box-shadow: 0px 5px 24px rgba(138, 143, 234, 0.12);
   backdrop-filter: blur(20px);
-  width: 337px;
-  height: 348px;
   flex-direction: column;
   border-radius: 25px;
   padding: 48px 32px;
+  max-width: 310px;
+  min-height: 320px;
 `;
 
-export const ProfilePic = styled.img`
+export const ENSNameCardImage = styled.img`
   background-position: center, center;
   background-size: cover;
-
+  margin-bottom: 15px;
   border-radius: 200px;
   height: 83px;
   width: 83px;
   z-index: 1;
 `;
 
-export const DomainText = styled.div`
-  ${NimiSignatureColor};
-  margin-top: auto;
-  font-family: 'Baloo 2';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 32px;
-  line-height: 100%;
-`;
-
-export const StyledButton = styled(Button)`
-  margin-top: auto;
+export const StyledDomainNameWrapper = styled.div`
+  width: 100%;
+  max-width: 200px;
+  text-align: center;
 `;
 
 export const StyledExternalLink = styled(ExternalLink)`
   ${NimiSignatureColor};
   margin-top: 26px;
+`;
+
+export const StyledDomainName = styled.h2`
+  ${NimiSignatureColor};
+  font-size: 32px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin-bottom: 32px;
+  line-height: 100%;
 `;
