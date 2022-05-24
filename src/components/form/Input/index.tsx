ebbce44fe41aa2f749b2986d2e14263c1b973e04
Adam Azad <adam@adamazad.com>
2022-05-24 09:17:22 +0100

    feat(#21): add Form components

---
+++ src/components/form/Input/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  padding: 12px 16px;
+  border: 2px solid #e6e8ec;
+  border-radius: 12px;
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    color: #b1b5c3;
+  }
+`;
---
+++ src/components/form/index.ts
@@ -0,0 +1,3 @@
+export { Input } from './Input';
+export { Label } from './Label';
+export { TextArea } from './TextArea';
