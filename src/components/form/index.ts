3c294fc1504c9695527c92f693cae88d806db64a
Adam Azad <adam@adamazad.com>
2022-05-28 10:38:39 +0100

    feat: add Checkbox component

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,3 +2,4 @@ export { Input } from './Input';
 export { Label } from './Label';
 export { TextArea } from './TextArea';
 export { FormGroup } from './FormGroup';
+export { Checkbox } from './Checkbox';
