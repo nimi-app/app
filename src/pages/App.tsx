d15848884ffd3f761ea30e949048f5648c2e34f4
Adam Azad <adam@adamazad.com>
2022-06-03 19:47:53 +0100

    feat(#35): add a buffer load while connecting the wallet

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,14 @@
 import { ApolloProvider } from '@apollo/client';
-import { Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import { Route, Routes } from 'react-router-dom';
 import styled, { useTheme } from 'styled-components';
 
 import { clients as subgraphClients, defaultClient as defaultSubgraphClient } from '../apollo/client';
+import { ReactComponent as NimiLogo } from '../assets/svg/nimi-logo-no-text.svg';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { WalletModal } from '../components/WalletModal';
-import { Web3Manager } from '../components/Web3Manager';
 import { useActiveWeb3React } from '../hooks/useWeb3';
 import { CreateNimiPage } from './CreateNimiPage';
 import { DomainsHome } from './Domains';
@@ -58,23 +58,57 @@ const AppMain = () => (
   </AppWrapper>
 );
 
+const AppLoaderContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+
+  > svg {
+    width: 100px;
+    width: 25vw;
+    max-width: 200px;
+  }
+`;
+
 export function App() {
-  const { chainId } = useActiveWeb3React();
+  const [isConnectingEagerly, setIsConnectingEagerly] = useState(true);
+  const { chainId, connector } = useActiveWeb3React();
   const theme = useTheme();
 
+  useEffect(() => {
+    if (!connector?.connectEagerly) {
+      return setIsConnectingEagerly(false);
+    }
+
+    connector.connectEagerly()?.then(() => setIsConnectingEagerly(false));
+    // eslint-disable-next-line
+  }, []);
+
+  if (isConnectingEagerly) {
+    return (
+      <AppLoaderContainer>
+        <NimiLogo />
+      </AppLoaderContainer>
+    );
+  }
+
   return (
-    <Suspense fallback={null}>
-      <SkeletonTheme baseColor={theme.bg3} highlightColor={theme.bg2}>
-        <ApolloProvider client={subgraphClients[chainId as number] || defaultSubgraphClient}>
-          <Web3Manager />
-          <WalletModal />
-          <Routes>
-            <Route element={<Landing />} path="/" />
-            <Route element={<AppMain />} path="domains/*" />
-            <Route element={<NotFound />} path="*" />
-          </Routes>
-        </ApolloProvider>
-      </SkeletonTheme>
-    </Suspense>
+    <SkeletonTheme baseColor={theme.bg3} highlightColor={theme.bg2}>
+      <ApolloProvider client={subgraphClients[chainId as number] || defaultSubgraphClient}>
+        <WalletModal />
+        <Routes>
+          <Route element={<Landing />} path="/" />
+          <Route element={<AppMain />} path="domains/*" />
+          <Route element={<NotFound />} path="*" />
+        </Routes>
+      </ApolloProvider>
+    </SkeletonTheme>
   );
 }
