24bacdd71d870585aba16bc16557c74398b6509c
Federico Luzzi <fedeluzzi00@gmail.com>
2022-06-03 16:45:43 +0200

    chore: adapt lens profile hook to graphql generator

--- src/hooks/useLensDefaultProfileData.ts
+++ src/hooks/useLensDefaultProfileData.ts
@@ -1,34 +1,7 @@
-import { gql } from '@apollo/client';
 import { useEffect, useState } from 'react';
 import { useActiveWeb3React } from './useWeb3';
+import { useGetDefaultProfileQuery } from '../generated/graphql/lens';
 import { useLensSubgraph } from './useLensSubgraph';
-
-const LENS_DEFAULT_PROFILE_QUERY = gql`
-  query getDefaultProfile($account: EthereumAddress!) {
-    defaultProfile(request: { ethereumAddress: $account }) {
-      name
-      bio
-      picture {
-        ... on NftImage {
-          uri
-        }
-        ... on MediaSet {
-          original {
-            url
-          }
-        }
-      }
-    }
-  }
-`;
-
-interface LensQueryResult {
-  defaultProfile: {
-    name: string;
-    bio: string;
-    picture: { uri: string } | { original: { url: string } };
-  } | null;
-}
 
 export interface LensDefaultProfileData {
   name: string;
@@ -39,39 +12,32 @@ export interface LensDefaultProfileData {
 export function useLensDefaultProfileData(): { loading: boolean; defaultProfileData: LensDefaultProfileData | null } {
   const { account } = useActiveWeb3React();
   const lensSubgraph = useLensSubgraph();
+  const { data, loading, error } = useGetDefaultProfileQuery({
+    client: lensSubgraph,
+    variables: {
+      account,
+    },
+  });
 
-  const [loading, setLoading] = useState(false);
+  console.log(error);
+
   const [defaultProfileData, setDefaultProfileData] = useState<LensDefaultProfileData | null>(null);
 
   useEffect(() => {
-    let cancelled = false;
-    const fetchData = async () => {
-      if (!!!account) return;
-      if (!cancelled) setLoading(true);
-      const {
-        data: { defaultProfile },
-      } = await lensSubgraph.query<LensQueryResult>({
-        query: LENS_DEFAULT_PROFILE_QUERY,
-        variables: { account },
-      });
-      if (!cancelled) setLoading(false);
-      if (!cancelled)
-        setDefaultProfileData(
-          defaultProfile
-            ? {
-                name: defaultProfile.name,
-                description: defaultProfile.bio,
-                pictureUrl:
-                  'uri' in defaultProfile.picture ? defaultProfile.picture.uri : defaultProfile.picture.original.url,
-              }
-            : null
-        );
-    };
-    fetchData();
-    return () => {
-      cancelled = true;
-    };
-  }, [lensSubgraph, account]);
+    if (!data) return;
+    setDefaultProfileData(
+      data.defaultProfile && data.defaultProfile.name && data.defaultProfile.bio && data.defaultProfile.picture
+        ? {
+            name: data.defaultProfile.name,
+            description: data.defaultProfile.bio,
+            pictureUrl:
+              'uri' in data.defaultProfile.picture
+                ? data.defaultProfile.picture.uri
+                : data.defaultProfile.picture?.original.url,
+          }
+        : null
+    );
+  }, [data, account]);
 
   return { loading, defaultProfileData };
 }
