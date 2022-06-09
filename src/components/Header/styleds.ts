cf633f6ee340f49855602f633352049ccaa868ce
Mi-Lan <git>
2022-06-09 23:37:41 +0200

    fix: Mobile nav fix

--- src/components/Header/styleds.ts
+++ src/components/Header/styleds.ts
@@ -1,16 +1,22 @@
-import { Flex } from 'rebass';
+import { Flex } from 'rebass/styled-components';
 import styled from 'styled-components';
-import { NimiSignatureColor } from '../../theme';
+import { MEDIA_WIDTHS, NimiSignatureColor } from '../../theme';
 
 export const HeaderWrapper = styled(Flex)`
   padding: 27px 90px;
   width: 100%;
   justify-content: space-between;
   align-items: center;
+  @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
+    padding: 27px 10px;
+  }
 `;
 export const AccountAndLinks = styled(Flex)`
   align-items: center;
   gap: 50px;
+  @media (max-width: ${MEDIA_WIDTHS.upToSmall}px) {
+    display: none;
+  }
 `;
 export const NavOption = styled.div`
   ${NimiSignatureColor};
