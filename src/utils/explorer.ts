74efd23e3b2b345ac13a7e0be66e51bef18aa290
Adam Azad <adam@adamazad.com>
2022-06-20 23:38:00 +0100

    chore: move explorer

---
+++ src/utils/explorer.ts
@@ -0,0 +1,33 @@
+const ETHERSCAN_PREFIXES: Record<number, string> = {
+  1: '',
+  4: 'rinkeby.',
+  5: 'goerli.',
+};
+
+function getExplorerPrefix(chainId: number) {
+  return `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`;
+}
+
+export function getEtherscanExplorerLink(
+  chainId: number,
+  data: string,
+  type: 'transaction' | 'token' | 'address' | 'block'
+): string {
+  const prefix = getExplorerPrefix(chainId);
+
+  switch (type) {
+    case 'transaction': {
+      return `${prefix}/tx/${data}`;
+    }
+    case 'token': {
+      return `${prefix}/token/${data}`;
+    }
+    case 'block': {
+      return `${prefix}/block/${data}`;
+    }
+    case 'address':
+    default: {
+      return `${prefix}/address/${data}`;
+    }
+  }
+}
