2203c4dd5b08f5936ba2cc05b3c0c8cf3324b45b
Adam Azad <adam@adamazad.com>
2022-08-01 23:24:12 +0100

    feat(card): add CardTitle

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -20,3 +20,9 @@ export const CardBody = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const CardTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 12px;
+`;
