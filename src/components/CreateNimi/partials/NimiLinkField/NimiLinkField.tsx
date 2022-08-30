97686ca0a5b1b2f3f56544f2cc57c32f42c9a9e9
Mi-Lan <git>
2022-08-30 02:04:59 +0200

    fix: Tidied up link field

--- src/components/CreateNimi/partials/NimiLinkField/NimiLinkField.tsx
+++ src/components/CreateNimi/partials/NimiLinkField/NimiLinkField.tsx
@@ -32,52 +32,50 @@ export function NimiLinkField({ link, index, content: defaultContent, title: def
   const [title, setTitle] = useState(defaultTitle);
   const [isInputFocused, setIsInputFocused] = useState(false);
   const [isValueValid, setIsValueValid] = useState(true);
+
+  //TODO: handle focused and error values for links
   console.log('isInputFocues', isInputFocused);
   console.log('isValid', isValueValid);
+
   const logo = useMemo(() => {
     return renderSVG(nimiLinkDetailsExtended[link].logo, 20);
   }, [link]);
-  // Handle the input change
-  const onLinkChange = (event) => {
-    // Extract the value from the event
-    const targetValue = event;
-    setValue(targetValue);
+
+  // Handle content change
+  const onLinkChange = (value) => {
+    setValue(value);
     // Vlidate
     nimiLinkValidator
       .isValid({
         type: link,
-        content: targetValue,
+        content: value,
       })
-      .then((isGut) => {
-        if (isGut) handleFormValue(targetValue);
+      .then((isValidLink) => {
+        if (isValidLink) handleFormValue(value);
 
-        setIsValueValid(isGut);
+        setIsValueValid(isValidLink);
       })
       .catch(() => setIsValueValid(false));
   };
+  //handle title change
+  const onTitleChange = (value) => {
+    setTitle(value);
 
-  const onTitleChange = (event) => {
-    // Extract the value from the event
-    const targetValue = event;
-    setTitle(targetValue);
-    console.log('setting', targetValue);
-    // Vlidate
+    const linksCurrentState = getFormValues('links') || [];
 
-    const linksPrevState = getFormValues('links') || [];
-
-    linksPrevState[index] = { type: link, title: targetValue, content: value };
-    setFormValue('links', linksPrevState);
+    linksCurrentState[index] = { type: link, title: value, content: linksCurrentState[index].content };
+    setFormValue('links', linksCurrentState);
   };
 
   useEffect(() => {
-    const linksPrevState = getFormValues('links') || [];
-    onTitleChange(linksPrevState[index].title);
+    //handle index change for the title
+    const linksCurrentState = getFormValues('links') || [];
+    onTitleChange(linksCurrentState[index].title);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [index, defaultTitle]);
   useEffect(() => {
-    //handle index change
-    // onLinkChange(defaultContent);
+    //handle index change for content
     onLinkChange(defaultContent);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -106,6 +104,8 @@ export function NimiLinkField({ link, index, content: defaultContent, title: def
     linksPrevState[index] = { type: link, title: linksPrevState[index].title, content: newValue };
     setFormValue('links', linksPrevState);
   };
+
+  //Removes link from the form
   const handleDelete = () => {
     const linksPrevState = getFormValues('links') || [];
 
@@ -113,14 +113,6 @@ export function NimiLinkField({ link, index, content: defaultContent, title: def
 
     setFormValue('links', linksPrevState);
   };
-
-  /**
-   * @todo - study the commented out code below and see if it can be used instead of the above -> Will do sensei
-   */
-  // useEffect(() => {
-  //   const valuechange = getFormValues('links').find((prevLink) => prevLink.type === link);
-  //   setValue(valuechange ? valuechange.content : value);
-  // }, [getFormValues, link, value]);
 
   return (
     <LinkFieldWrapper>
