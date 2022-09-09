527f21097d1baf16b1d27ceaf1abeafdeaa71617
Basic Mirko <bejzik8@gmail.com>
2022-09-10 01:32:15 +0200

    Working on modal's body.

--- src/components/CreateNimi/partials/ConfigurePOAPsModal/ConfigurePOAPsModal.tsx
+++ src/components/CreateNimi/partials/ConfigurePOAPsModal/ConfigurePOAPsModal.tsx
@@ -11,22 +11,28 @@ export function ConfigurePOAPsModal() {
         <ModalSubtitle>Add your POAPs in the order you want to showcase them.</ModalSubtitle>
         <CloseButton />
       </Header>
+      <Body>
+        <BodyControls>
+          <BodyTitle>POAPs</BodyTitle>
+          <BodyNavigation></BodyNavigation>
+        </BodyControls>
+      </Body>
     </Modal>
   );
 }
 
 const Modal = styled.div`
   width: 620px;
-  height: 400px;
   padding: 32px;
   border-radius: 24px;
   background-color: white;
 
-  background-color: yellow;
+  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.1);
 `;
 
-const Header = styled.div`
+const Header = styled.header`
   position: relative;
+  margin-bottom: 24px;
 `;
 
 const ModalTitle = styled.h1`
@@ -47,4 +53,30 @@ const CloseButton = styled(CloseIcon)`
   top: 0;
   right: 0;
   cursor: pointer;
+`;
+
+const Body = styled.main`
+  width: 100%;
+  padding: 36px;
+  border-radius: 12px;
+  background-color: #f1f2f5;
+`;
+
+const BodyControls = styled.div`
+  height: 38px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: red;
+`;
+
+const BodyTitle = styled.h2`
+  line-height: 26px;
+  font-size: 26px;
+  color: black;
+`;
+
+const BodyNavigation = styled.nav`
+  height: 26px;
 `;
