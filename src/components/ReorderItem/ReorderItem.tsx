07809be77f36c0b2e19a590f2794a80f901e9fb0
Basic Mirko <bejzik8@gmail.com>
2022-09-15 02:53:15 +0200

    Updated ReorderingItem component.

--- src/components/ReorderItem/ReorderItem.tsx
+++ src/components/ReorderItem/ReorderItem.tsx
@@ -1,19 +1,40 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
 import { Reorder } from 'framer-motion/dist/framer-motion';
 
-export const ReorderItem = ({ value }) => {
+import { ReactComponent as DragDotsSVG } from '../../assets/svg/dragdots.svg';
+
+type ReorderItemProps = {
+  value: string;
+  children: ReactNode;
+};
+
+export const ReorderItem = ({ value, children }: ReorderItemProps) => {
   return (
     <Reorder.Item
       value={value}
       as="div"
       style={{
         width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative',
         padding: '15px 15px 15px 39px',
         borderRadius: '12px',
         backgroundColor: '#F1F1F1',
         marginBottom: '20px',
+        cursor: 'pointer',
       }}
     >
-      {value}
+      {children}
+      <DraggingDots />
     </Reorder.Item>
   );
 };
+
+const DraggingDots = styled(DragDotsSVG)`
+  position: absolute;
+  left: 15px;
+  top: 50%;
+  transform: translate(0, -50%);
+`;
