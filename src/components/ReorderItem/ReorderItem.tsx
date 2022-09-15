b66d66707639966e5f7d874a2bbed0d84cbdc368
Basic Mirko <bejzik8@gmail.com>
2022-09-15 02:42:10 +0200

    Updated ReorderItem component.

--- src/components/ReorderItem/ReorderItem.tsx
+++ src/components/ReorderItem/ReorderItem.tsx
@@ -2,7 +2,17 @@ import { Reorder } from 'framer-motion/dist/framer-motion';
 
 export const ReorderItem = ({ value }) => {
   return (
-    <Reorder.Item value={value} as="div">
+    <Reorder.Item
+      value={value}
+      as="div"
+      style={{
+        width: '100%',
+        padding: '15px 15px 15px 39px',
+        borderRadius: '12px',
+        backgroundColor: '#F1F1F1',
+        marginBottom: '20px',
+      }}
+    >
       {value}
     </Reorder.Item>
   );
