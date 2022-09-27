19a48cd278af5943671b1c2ecf9645883b51c942
Mirko Basic <bejzik8@gmail.com>
2022-09-27 18:49:12 +0200

    Fixed pen presentation.

--- src/components/ReorderInput/ReorderInput.tsx
+++ src/components/ReorderInput/ReorderInput.tsx
@@ -53,7 +53,7 @@ export function ReorderInput({ value, updateLink, removeLink }: ReorderInputProp
             (_, firstLetter, restOfTheWord) => firstLetter + restOfTheWord.toLowerCase()
           )}
         />
-        <Pen />
+        {!content && <PenComponent type={type} />}
         {title && <ClearButton onClick={() => updateLink(value.id!, 'title', '')} />}
       </InputContainer>
       <InputContainer>
@@ -72,15 +72,6 @@ export function ReorderInput({ value, updateLink, removeLink }: ReorderInputProp
   );
 }
 
-const Pen = styled(PenSVG)`
-  display: none;
-  position: absolute;
-  right: 10px;
-  top: 50%;
-  transform: translate(0, -50%);
-  cursor: pointer;
-`;
-
 const SharedInputStyles = css<{ inputInvalid?: boolean }>`
   width: 100%;
   line-height: 22px;
@@ -92,10 +83,6 @@ const SharedInputStyles = css<{ inputInvalid?: boolean }>`
   outline: none;
   transition: all 0.1s linear;
 
-  &:hover:not(:focus) + ${Pen} {
-    display: block;
-  }
-
   &:focus {
     background-color: white;
     font-size: 18px;
@@ -105,9 +92,6 @@ const SharedInputStyles = css<{ inputInvalid?: boolean }>`
     & + svg {
       visibility: visible;
       opacity: 1;
-    }
-    ${Pen} {
-      display: none;
     }
 
     color: #5274ff;
@@ -138,6 +122,10 @@ const TitleInput = styled.input`
   padding: 8px 30px 8px 20px;
   ${SharedInputStyles}
   background-color: #f1f1f1;
+
+  &:hover:not(:focus) + .pen-component {
+    display: flex;
+  }
 `;
 
 const ContentInput = styled.input<{ inputInvalid: boolean }>`
@@ -163,6 +151,34 @@ const ClearButton = styled(XSVG)<{ right?: string }>`
   &:hover path {
     fill: #8c90a0;
   }
+`;
+
+const PenComponent = ({ type }) => (
+  <PenContainer className="pen-component">
+    <PenPlaceholder>
+      {type.replace(/(^\w)(\S*)/g, (_, firstLetter, restOfTheWord) => firstLetter + restOfTheWord.toLowerCase())}
+    </PenPlaceholder>
+    <Pen />
+  </PenContainer>
+);
+
+const PenContainer = styled.div`
+  display: none;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 30px;
+  transform: translate(0, -50%);
+`;
+
+const PenPlaceholder = styled.p`
+  display: inline-block;
+  visibility: hidden;
+`;
+
+const Pen = styled(PenSVG)`
+  display: inline-block;
+  cursor: pointer;
 `;
 
 const LogoContainer = styled.div`
