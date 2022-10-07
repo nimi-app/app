eb15db682bc6f200680d7cca83d389c4d1b7b572
Mirko Basic <bejzik8@gmail.com>
2022-10-07 16:17:47 -0500

    Created TemplatePickerButton component.

---
+++ src/components/TemplatePickerButton/TemplatePickerButton.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export function TemplatePickerButton() {
+  return <Container>123</Container>;
+}
+
+const Container = styled.button`
+  width: 170px;
+  height: 150px;
+  border-radius: 13px;
+  border: 5px solid #fff;
+  background-color: #f0f3fb;
+`;
