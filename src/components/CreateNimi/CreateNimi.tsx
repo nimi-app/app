d533be788bf4d62c37760b4727a0de9cc50c789a
Violet <git>
2022-10-07 15:40:23 -0500

    feature: Poap theme hook

--- src/components/CreateNimi/CreateNimi.tsx
+++ src/components/CreateNimi/CreateNimi.tsx
@@ -196,7 +196,6 @@ export function CreateNimi({ ensAddress, ensName, provider }: CreateNimiProps) {
       setPublishNimiError(undefined);
       setIsNimiPublished(false);
     });
-    console.log('data', data);
 
     try {
       if (!publicResolverContract) {
@@ -256,7 +255,7 @@ export function CreateNimi({ ensAddress, ensName, provider }: CreateNimiProps) {
       });
     }
   };
-  console.log('formwatch', formWatchPayload);
+
   const onSubmitInvalid = (data) => {
     console.log('SUBMIT INVALID', data);
   };
