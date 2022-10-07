5063f63c529d52eb1bb636e554340010ee484e02
Mirko Basic <bejzik8@gmail.com>
2022-10-07 11:49:03 -0500

    Updated TemplatePickerModal component.

--- src/components/CreateNimi/partials/TemplatePickerModal/TemplatePickerModal.tsx
+++ src/components/CreateNimi/partials/TemplatePickerModal/TemplatePickerModal.tsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import styled from 'styled-components';
 
 import { ModalBase } from '../ModalBase';
+import { TemplateItem } from './TemplateItem';
+import NimiLogo from '../../../../assets/svg/nimi-logo-no-text.svg';
+import NimiOG from '../../../../assets/svg/nimi-og.svg';
+import devconImageLogo from '../../../../assets/svg/devcon-image-logo.svg';
+import DevconLogoText from '../../../../assets/svg/devcon-text.svg';
 
 export function TemplatePickerModal() {
   const [modalContainer] = useState(() => document.createElement('div'));
@@ -22,8 +28,15 @@ export function TemplatePickerModal() {
       subtitle="Here is the selection of template you can choose from. To unlock more templates, collect POAPs and NFTs"
       handleCloseModal={() => console.log('CLOSE MODAL')}
     >
-      <div>123</div>
+      <Container>
+        <TemplateItem themeImageLogo={NimiLogo} themeNameLogo={NimiOG} />
+        <TemplateItem themeImageLogo={devconImageLogo} themeNameLogo={DevconLogoText} />
+      </Container>
     </ModalBase>,
     modalContainer
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+`;
