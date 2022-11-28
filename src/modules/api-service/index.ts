e5a32efa4bcda82035219c7e74eb9bb33ae3f58b
Violet <git>
2022-11-28 23:22:45 +0100

    fix: get abi function

--- src/modules/api-service/index.ts
+++ src/modules/api-service/index.ts
@@ -8,7 +8,9 @@ export type NimiApiVersion = 'v1.4' | 'v1';
  * @returns {string} The base URL for the Nimi API.
  */
 export function getAPIBaseURL() {
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  if (!process.env.REACT_APP_NIMI_API_BASE_URL) {
+    throw new Error('REACT_APP_NIMI_API_BASE_URL is not set.');
+  } else if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     return process.env.REACT_APP_NIMI_API_DEV_BASE_URL;
   } else {
     return process.env.REACT_APP_NIMI_API_BASE_URL as string;
