f3ef6e13932f77fcd357bef0348911acbb2034a2
bejzik8 <bejzik8@gmail.com>
2022-12-13 02:02:45 +0100

    Updated providers.

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,28 @@
 import './i18n';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider as ReduxStoreProvider } from 'react-redux';
 
 import { App } from './pages/App';
-import { store } from './state';
+import { ReactQueryProvider, ReduxProvider } from './providers';
 import { FixedGlobalStyle, ThemedGlobalStyle, ThemeProvider } from './theme';
 
 if ('ethereum' in window) {
   (window.ethereum as any).autoRefreshOnNetworkChange = false;
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: { refetchOnWindowFocus: false },
-  },
-});
-
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
-    <ReduxStoreProvider store={store}>
-      <QueryClientProvider client={queryClient}>
+    <ReduxProvider>
+      <ReactQueryProvider>
         <FixedGlobalStyle />
         <ThemeProvider>
           <ThemedGlobalStyle />
           <App />
         </ThemeProvider>
-      </QueryClientProvider>
-    </ReduxStoreProvider>
+      </ReactQueryProvider>
+    </ReduxProvider>
   </StrictMode>
 );
