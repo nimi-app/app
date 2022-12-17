76a02bf81e46a33fe99b18968a594e7fb1a3f416
bejzik8 <bejzik8@gmail.com>
2022-12-17 03:13:48 +0100

    Updated DomainsHome page.

--- src/pages/DomainsHome/index.tsx
+++ src/pages/DomainsHome/index.tsx
@@ -16,77 +16,27 @@ import { useGetENSDomainsByAddress } from '../../hooks/useGetENSDomainsByAddress
 import { useRainbow } from '../../hooks/useRainbow';
 import { NimiSignatureColor } from '../../theme';
 
-const StyledDomainsWrapper = styled(Flex)`
-  flex-wrap: wrap;
-  gap: 18px;
-  justify-content: start;
-`;
-const ErrorContainer = styled.div`
-  ${NimiSignatureColor};
-  font-weight: 800;
-  font-size: 36px;
-  line-height: 39px;
-  margin-bottom: 36px;
-`;
-const DomainsHeader = styled.div`
-  ${NimiSignatureColor};
-  font-weight: 800;
-  font-size: 36px;
-  line-height: 39px;
-  margin-bottom: 36px;
-  width: fit-content;
-  display: flex;
-`;
-const AddDomain = styled(DottedButtonBase)`
-  width: 308px;
-  border-radius: 16px;
-  height: 146px;
-  font-weight: 400;
-  letter-spacing: -0.02em;
-  font-size: 24px;
-`;
-const BigBanner = styled(DottedButtonBase)`
-  border-radius: 16px;
-  font-weight: 400;
-  font-size: 24px;
-  width: 100%;
-  padding: 80px 0;
-  letter-spacing: -0.02em;
-`;
+export function DomainsHome() {
+  const { chainId, account, isConnected } = useRainbow();
+  const { t } = useTranslation(['common', 'landing']);
+  const navigate = useNavigate();
 
-const BuyDomainLink = styled.p`
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 22px;
-  text-decoration-line: underline;
-  text-decoration-color: #9280e3;
-  margin-top: 17px;
-  cursor: pointer;
-`;
-const TopSection = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-const StyledInput = styled(InputFieldWithIcon)`
-  max-width: 200px !important;
-  display: flex !important;
-  align-items: flex-start;
-`;
-const LoaderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  padding: 80px 0;
-`;
+  if (isConnected !== true) {
+    navigate('/');
+    return <Container />;
+  }
 
-const NormalText = styled.p`
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 22px;
-  margin-top: 17px;
-  cursor: pointer;
-`;
+  if (ENV_SUPPORTED_CHAIN_IDS.includes(chainId as number) === false) {
+    return (
+      <Container>
+        <ErrorContainer>{t('error.unsupportedNetwork')}</ErrorContainer>
+        <NormalText>Please change your network by clicking the account button on the top right.</NormalText>
+      </Container>
+    );
+  }
+
+  return <Domains address={account as string} />;
+}
 
 interface DomainsProps {
   address: string;
@@ -95,6 +45,7 @@ interface DomainsProps {
 function Domains({ address }: DomainsProps) {
   const [searchText, setSearchText] = useState('');
   const [page, setPage] = useState(0);
+
   const { data: domainList, loading, hasNextPage } = useGetENSDomainsByAddress(address, page, searchText);
   const { t } = useTranslation('nimi');
 
@@ -145,24 +96,81 @@ function Domains({ address }: DomainsProps) {
   );
 }
 
-/**
- * A logic wrapper around Domains components
- */
-export function DomainsHome() {
-  const { chainId, account, isConnected } = useRainbow();
-  const { t } = useTranslation(['common', 'landing']);
-  const navigate = useNavigate();
-  if (isConnected !== true) {
-    navigate('/');
-    return <Container />;
-  }
-  if (ENV_SUPPORTED_CHAIN_IDS.includes(chainId as number) === false) {
-    return (
-      <Container>
-        <ErrorContainer>{t('error.unsupportedNetwork')}</ErrorContainer>
-        <NormalText>Please change your network by clicking the account button on the top right.</NormalText>
-      </Container>
-    );
-  }
-  return <Domains address={account as string} />;
-}
+const StyledDomainsWrapper = styled(Flex)`
+  flex-wrap: wrap;
+  gap: 18px;
+  justify-content: start;
+`;
+
+const ErrorContainer = styled.div`
+  ${NimiSignatureColor};
+  font-weight: 800;
+  font-size: 36px;
+  line-height: 39px;
+  margin-bottom: 36px;
+`;
+
+const DomainsHeader = styled.h1`
+  display: flex;
+  width: fit-content;
+  line-height: 40px;
+  font-size: 36px;
+  font-weight: 800;
+  ${NimiSignatureColor};
+  margin-bottom: 36px;
+`;
+
+const AddDomain = styled(DottedButtonBase)`
+  width: 308px;
+  border-radius: 16px;
+  height: 146px;
+  font-weight: 400;
+  letter-spacing: -0.02em;
+  font-size: 24px;
+`;
+
+const BigBanner = styled(DottedButtonBase)`
+  border-radius: 16px;
+  font-weight: 400;
+  font-size: 24px;
+  width: 100%;
+  padding: 80px 0;
+  letter-spacing: -0.02em;
+`;
+
+const BuyDomainLink = styled.p`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 22px;
+  text-decoration-line: underline;
+  text-decoration-color: #9280e3;
+  margin-top: 17px;
+  cursor: pointer;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const StyledInput = styled(InputFieldWithIcon)`
+  max-width: 200px !important;
+  display: flex !important;
+  align-items: flex-start;
+`;
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 80px 0;
+`;
+
+const NormalText = styled.p`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 22px;
+  margin-top: 17px;
+  cursor: pointer;
+`;
