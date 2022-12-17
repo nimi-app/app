309b60fdde0cb7d8c15ea7dfe48a09b1ae3409aa
bejzik8 <bejzik8@gmail.com>
2022-12-17 02:04:20 +0100

    Updated Header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,33 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { ReactComponent as NimiLogo } from '../../assets/svg/nimi-logo-text.svg';
+import { MEDIA_WIDTHS } from '../../theme';
 import { Web3Status } from '../Web3Status';
-import { HeaderWrapper } from './styleds';
 
 export function Header() {
   return (
-    <HeaderWrapper>
-      <Link to="/">
-        <NimiLogo width="100px" height="40px" />
-      </Link>
-      <Web3Status />
-    </HeaderWrapper>
+    <Container>
+      <Content>
+        <Link to="/domains/all">
+          <NimiLogo width="100px" height="40px" />
+        </Link>
+        <Web3Status />
+      </Content>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  max-width: ${MEDIA_WIDTHS.upToMedium}px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  margin: 0 auto;
+`;
