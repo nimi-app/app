2ba13f3a68f9b28f6c53d85ee83a416e56afc9d4
bejzik8 <bejzik8@gmail.com>
2022-12-17 23:44:38 +0100

    Updated Heading component.

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { NimiSignatureColor } from '../../theme';
 
@@ -6,31 +6,43 @@ type HeadingProps = {
   children: string;
   type?: 'main' | 'sub';
   color?: string;
+  marginBottom?: string;
 };
 
-export function Heading({ children, type = 'main', color }: HeadingProps) {
-  if (type === 'sub') return <SubHeading color={color}>{children}</SubHeading>;
-  return <MainHeading color={color}>{children}</MainHeading>;
+export function Heading({ children, type = 'main', color, marginBottom }: HeadingProps) {
+  if (type === 'sub')
+    return (
+      <SubHeading color={color} marginBottom={marginBottom}>
+        {children}
+      </SubHeading>
+    );
+  return (
+    <MainHeading color={color} marginBottom={marginBottom}>
+      {children}
+    </MainHeading>
+  );
 }
 
 type StyledHeadingProps = {
   color?: string;
+  marginBottom?: string;
 };
 
-export const MainHeading = styled.h1<StyledHeadingProps>`
+const SharedProps = css<StyledHeadingProps>`
+  ${({ color }) => (color ? `color: ${color};` : `${NimiSignatureColor}`)}
+  ${({ marginBottom }) => marginBottom && `margin-bottom: ${marginBottom};`}
+`;
+
+export const MainHeading = styled.h1`
   line-height: 40px;
   font-size: 36px;
   font-weight: 800;
-  margin-bottom: 16px;
-
-  ${({ color }) => (color ? `color: ${color};` : `${NimiSignatureColor}`)}
+  ${SharedProps}
 `;
 
-export const SubHeading = styled.h2<StyledHeadingProps>`
+export const SubHeading = styled.h2`
   line-height: 24px;
   font-size: 20px;
   font-weight: 600;
-  margin-bottom: 16px;
-
-  ${({ color }) => (color ? `color: ${color};` : `${NimiSignatureColor}`)}
+  ${SharedProps}
 `;
