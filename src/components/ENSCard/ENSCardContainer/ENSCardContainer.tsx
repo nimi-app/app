047944870596400f6d1ba911da55bfeb75983124
bejzik8 <bejzik8@gmail.com>
2022-12-21 04:59:45 +0100

    Updated ENSCardContainer component.

--- src/components/ENSCard/ENSCardContainer/ENSCardContainer.tsx
+++ src/components/ENSCard/ENSCardContainer/ENSCardContainer.tsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import PurpleCircle from '../../../assets/svg/purpleCircle.svg';
-import { useENSMetadata } from '../../../hooks/useENSMetadata';
+// import { useENSMetadata } from '../../../hooks/useENSMetadata';
 import { useNimiData } from '../../../hooks/useNimiData';
 import { ENSDomain } from '../../../models';
 import { ENSNameCardImage, StyledDomainName, StyledENSNameCardWrapper } from '../styleds';
@@ -16,34 +16,20 @@ export function ENSCardContainer({ domain }: ENSCardContainerProps) {
   const ref = useRef(null);
   const isInView = useInView(ref);
 
-  const { data: metadata, loading: metadataLoading } = useENSMetadata(domain.name!);
+  // TODO: MIRKO CHECK IF useENSMetadata HOOK IS NECESSARY
+  // const { data: metadata, loading: metadataLoading } = useENSMetadata(domain.name!);
   const { data: domainData } = useNimiData(domain?.name, isInView);
 
-  if (domainData !== null && domainData !== undefined) {
-    return (
-      <Link to={`/domains/${domain.name}`} state={domainData}>
-        <StyledENSNameCardWrapper>
-          <ENSNameCardImage
-            alt={domainData.nimi.ensAddress}
-            src={domainData.nimi.image ? domainData.nimi.image.url : PurpleCircle}
-          />
-          <StyledDomainName>{domain.name}</StyledDomainName>;
-        </StyledENSNameCardWrapper>
-      </Link>
-    );
-  }
-
   return (
-    <Link ref={ref} to={`/domains/${domain.name}`}>
+    <Link ref={ref} to={`/domains/${domain.name}`} state={domainData}>
       <StyledENSNameCardWrapper>
         <ENSNameCardImage
           alt={`${domain.name} Image`}
-          src={metadataLoading ? PurpleCircle : metadata ? metadata.image : PurpleCircle}
+          src={domainData?.nimi?.image ? domainData.nimi.image.url : PurpleCircle}
           onError={(event) => {
             const target = event.target as HTMLImageElement;
-            if (target.src !== PurpleCircle) {
-              target.src = PurpleCircle;
-            }
+
+            if (target.src !== PurpleCircle) target.src = PurpleCircle;
           }}
         />
         <StyledDomainName>{domain.name}</StyledDomainName>
