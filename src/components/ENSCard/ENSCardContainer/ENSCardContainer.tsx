381eee1fdfe2b3b1bd59816278dbcd2c01615816
bejzik8 <bejzik8@gmail.com>
2022-12-21 12:02:49 +0100

    Updated ENSCardContainer component.

--- src/components/ENSCard/ENSCardContainer/ENSCardContainer.tsx
+++ src/components/ENSCard/ENSCardContainer/ENSCardContainer.tsx
@@ -1,12 +1,13 @@
 import { useInView } from 'framer-motion';
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import PurpleCircle from '../../../assets/svg/purpleCircle.svg';
 // import { useENSMetadata } from '../../../hooks/useENSMetadata';
 import { useNimiData } from '../../../hooks/useNimiData';
 import { ENSDomain } from '../../../models';
-import { ENSNameCardImage, StyledDomainName, StyledENSNameCardWrapper } from '../styleds';
+import { NimiSignatureColor } from '../../../theme';
 
 type ENSCardContainerProps = {
   domain: ENSDomain;
@@ -16,7 +17,7 @@ export function ENSCardContainer({ domain }: ENSCardContainerProps) {
   const ref = useRef(null);
   const isInView = useInView(ref);
 
-  // TODO: MIRKO CHECK IF useENSMetadata HOOK IS NECESSARY
+  // TODO: MIRKO CHECK IF useENSMetadata HOOK IS NECESSARY OVER HERE AND AT ALL
   // const { data: metadata, loading: metadataLoading } = useENSMetadata(domain.name!);
   const { data: domainData } = useNimiData(domain?.name, isInView);
 
@@ -37,3 +38,44 @@ export function ENSCardContainer({ domain }: ENSCardContainerProps) {
     </Link>
   );
 }
+
+export const StyledENSNameCardWrapper = styled.div`
+  width: 308px;
+  height: 146px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: row;
+  border-radius: 25px;
+  background: #fff;
+  box-shadow: 0px 5px 24px rgba(138, 143, 234, 0.12);
+  padding: 42px 22px;
+  cursor: pointer;
+  overflow: hidden;
+`;
+
+export const ENSNameCardImage = styled.img`
+  background-position: center center;
+  background-size: cover;
+  border-radius: 200px;
+  height: 83px;
+  width: 83px;
+  z-index: 1;
+  box-shadow: 0px 5px 24px rgba(138, 143, 234, 0.12);
+  backdrop-filter: blur(20px);
+`;
+
+export const StyledDomainName = styled.div`
+  ${NimiSignatureColor};
+  font-size: 24px;
+  overflow: hidden;
+  line-height: 26px;
+  white-space: wrap;
+  margin-left: 16px;
+  text-align: start;
+  text-overflow: ellipsis;
+  line-height: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+`;
