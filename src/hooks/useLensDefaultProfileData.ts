a6b67c5ee4cb684d8d8638d4ae86f0a54d4d67bf
Violet <milanrasovic.f@gmail.com>
2022-12-26 21:38:10 +0100

    fix: Added chainId to lens arguments

--- src/hooks/useLensDefaultProfileData.ts
+++ src/hooks/useLensDefaultProfileData.ts
@@ -14,6 +14,7 @@ export function useLensDefaultProfileData(): { loading: boolean; defaultProfileD
     GraphQlClientDynamic(chainId, GRAPH_ENDPOINT.LENS),
     {
       account,
+      chainId,
     },
     {
       select: ({ defaultProfile }) => {
