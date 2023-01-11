626b3b3b11472e3ed34bcc51210153ef36879809
Mirko Basic <bejzik8@gmail.com>
2023-01-11 01:22:53 +0100

    Updated domain page.

--- src/pages/domains/[domain]/index.tsx
+++ src/pages/domains/[domain]/index.tsx
@@ -1,18 +1,28 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+import { Container } from '../../../components/Container';
+import { CreateNimi } from '../../../components/CreateNimi';
 import { CreateNimiContainer } from '../../../components/CreateNimi/CreateNimiContainer';
+import { Loader } from '../../../components/Loader';
+import { useAvaliableThemesFromPoaps } from '../../../hooks/useAvaliableThemesFromPoaps';
+import { useInitialtNimiData } from '../../../hooks/useDefaultNimiData';
+import { useRainbow } from '../../../hooks/useRainbow';
 import { PageLayout } from '../../../layout';
+import { insertPoapWidgetIntoNimi } from '../../../utils';
 
 export default function CreateNimiPage() {
+  const { account } = useRainbow();
+
   const router = useRouter();
   const { domain } = router.query;
 
-  // DOn't render anything on server side
-  if (typeof window === 'undefined') {
-    console.log('Server side rendering');
-    return null;
-  }
+  const { avaliableThemes, isLoading: isThemeLoading, hasPoaps } = useAvaliableThemesFromPoaps(account);
+
+  const { data: initialNimi, loading: initialNimiLoading } = useInitialtNimiData({
+    ensName: domain as string,
+    account: account as string,
+  });
 
   return (
     <>
@@ -20,7 +30,18 @@ export default function CreateNimiPage() {
         <title>Create Nimi for {domain}</title>
       </Head>
       <PageLayout>
-        <CreateNimiContainer ensName={domain as string} />
+        <Container>
+          {initialNimiLoading || initialNimi === undefined || isThemeLoading ? (
+            <Loader />
+          ) : (
+            <CreateNimi
+              ensAddress={account as string}
+              ensName={domain as string}
+              availableThemes={avaliableThemes}
+              initialNimi={insertPoapWidgetIntoNimi(initialNimi, hasPoaps, account)}
+            />
+          )}
+        </Container>
       </PageLayout>
     </>
   );
