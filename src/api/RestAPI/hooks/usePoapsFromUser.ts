f27fdc3b760828191657eea0c9024452e5dd56c5
Adam Azad <adam@adamazad.com>
2023-01-17 23:06:17 +0000

    chore(hooks): clean up POAP hooks

--- src/api/RestAPI/hooks/usePoapsFromUser.ts
+++ src/api/RestAPI/hooks/usePoapsFromUser.ts
@@ -1,29 +1,12 @@
+import { POAPToken } from '@nimi.io/card/types';
 import { useQuery } from '@tanstack/react-query';
 
 import { getPOAPAPIClient } from '../utils';
 
-type Poap = {
-  city: string;
-  country: string;
-  desciption: string;
-  end_date: string;
-  event_url?: string;
-  expiry_date: string;
-  fancy_id: string;
-  id: number;
-  image_url: string;
-  name: string;
-  start_date: string;
-  supply: number;
-  year: number;
-};
-
-export interface PoapData {
-  chain: string;
-  created: string;
-  event: Poap;
-  owner: string;
-  tokenId: string;
+export async function fetchUserPOAPs(account: string) {
+  const poapClient = getPOAPAPIClient();
+  const { data } = await poapClient.get<POAPToken[]>(`/actions/scan/${account.toLowerCase()}`);
+  return data;
 }
 
 /**
@@ -31,12 +14,8 @@ export interface PoapData {
  */
 export function usePoapsFromUser(account?: string) {
   const getPoapsFromUser = async () => {
-    if (!account) {
-      return [];
-    }
-    const poapClient = getPOAPAPIClient();
-    const { data } = await poapClient.get<PoapData[]>(`/actions/scan/${account.toLowerCase()}`);
-    return data;
+    if (!account) return [];
+    return fetchUserPOAPs(account);
   };
 
   return useQuery(['fetchUserPoaps', account], getPoapsFromUser);
