96d3543460b952e3849deb45ee969b8ae9bae1f6
Violet <milanrasovic.f@gmail.com>
2023-01-22 22:55:41 +0100

    fix: Address error and added useFieldArray

--- src/components/CreateNimi/partials/NimiBlockchainField/NimiBlockchainField.tsx
+++ src/components/CreateNimi/partials/NimiBlockchainField/NimiBlockchainField.tsx
@@ -1,8 +1,7 @@
 import { NIMI_BLOCKCHAIN_LOGO_URL } from '@nimi.io/card/constants';
-import { Nimi, NimiBlockchain } from '@nimi.io/card/types';
+import { NimiBlockchain, NimiBlockchainAddress } from '@nimi.io/card/types';
 import { validateNimiBlockchainAddress } from '@nimi.io/card/validators';
 import { ChangeEventHandler, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import { InputFieldWithIcon } from '../../../Input';
@@ -10,18 +9,19 @@ import { InputFieldWithIcon } from '../../../Input';
 export interface NimiBlockchainFieldProps {
   blockchain: NimiBlockchain;
   index: number;
+  removeAddress: (index: number) => void;
+  updateAddress: (index: number, value: NimiBlockchainAddress) => void;
 }
 
 /**
  * Handles the input for blockchain address
  */
-export function NimiBlockchainField({ blockchain, index }: NimiBlockchainFieldProps) {
+export function NimiBlockchainField({ updateAddress, removeAddress, blockchain, index }: NimiBlockchainFieldProps) {
   const { t } = useTranslation('nimi');
   const label = t(`formLabel.${blockchain.toLowerCase()}`);
 
-  const { setValue: setAddressValue, getValues } = useFormContext<Nimi>();
   const [value, setValue] = useState('');
-  const [isValueValid, setIsValueValid] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   //checks if address is valid and submits it to form if its
   const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -35,38 +35,32 @@ export function NimiBlockchainField({ blockchain, index }: NimiBlockchainFieldPr
     })
       .then((isValidAddress) => {
         if (isValidAddress) {
-          const addressPrevState = getValues('addresses') || [];
-          addressPrevState[index] = {
+          updateAddress(index, {
             blockchain,
             address: targetValue,
-          };
-          setAddressValue('addresses', addressPrevState);
-          setIsValueValid(true);
+          });
+          console.log('validation passed');
+          setIsError(false);
         } else {
-          setIsValueValid(false);
+          console.log('validation failed');
+          setIsError(true);
         }
       })
-      .catch(() => setIsValueValid(false));
-  };
-
-  //deletes address from the form
-  const onDelete = () => {
-    const addressesState = getValues('addresses') || [];
-
-    addressesState.splice(index, 1);
-
-    setAddressValue('addresses', addressesState);
+      .catch((e) => {
+        console.log('validation failed catch block', e);
+        setIsError(true);
+      });
   };
 
   return (
     <InputFieldWithIcon
       inputLogo={NIMI_BLOCKCHAIN_LOGO_URL[blockchain]}
-      isInvalidInput={isValueValid}
+      isInvalidInput={isError}
       onChange={onChange}
       placeholder={`${label} address`}
       content={value}
       onClearClick={() => setValue('')}
-      onInputClick={onDelete}
+      onInputClick={() => removeAddress(index)}
       // TODO: Add id to blockchainAddress in the card
       id={blockchain + index}
     />
