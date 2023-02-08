eee58ba764df43b62e3dc1899848c0baffe6cc76
Adam Azad <adam@adamazad.com>
2023-02-08 14:21:31 +0000

    test: cleanup
    test: use full path

--- .github/workflows/setup-ssh.sh
+++ .github/workflows/setup-ssh.sh
@@ -8,17 +8,10 @@ if [ -z "$SSH_PUBLIC_KEY" ]; then
     exit 1
 fi
 
-# Install git and ssh
-apt-get update && \
-  apt-get install -y \
-  git \
-  openssh-server
 # Authorize SSH Host
-mkdir -p /root/.ssh && \
-  chmod 0700 /root/.ssh && \
-  ssh-keyscan github.com > /root/.ssh/known_hosts
+ssh-keyscan github.com > /home/runner/.ssh/known_hosts
 # Add the keys and set permissions
-echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa && \
-  echo "$SSH_PUBLIC_KEY" > /root/.ssh/id_rsa.pub && \
-  chmod 600 /root/.ssh/id_rsa && \
-  chmod 600 /root/.ssh/id_rsa.pub
+echo "$SSH_PRIVATE_KEY" >  /home/runner/.ssh/id_rsa && \
+  echo "$SSH_PUBLIC_KEY" > /home/runner/.ssh/id_rsa.pub && \
+  chmod 600 /home/runner/.ssh/id_rsa && \
+  chmod 600 /home/runner/.ssh/id_rsa.pub
