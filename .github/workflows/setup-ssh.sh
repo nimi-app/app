effedb5715de0aa59ccc2d404d3602034a73a170
Adam Azad <adam@adamazad.com>
2023-02-08 14:24:14 +0000

    named steps

--- .github/workflows/setup-ssh.sh
+++ .github/workflows/setup-ssh.sh
@@ -9,9 +9,9 @@ if [ -z "$SSH_PUBLIC_KEY" ]; then
 fi
 
 # Authorize SSH Host
-ssh-keyscan github.com > /home/runner/.ssh/known_hosts
+ssh-keyscan github.com > ~/.ssh/known_hosts
 # Add the keys and set permissions
-echo "$SSH_PRIVATE_KEY" >  /home/runner/.ssh/id_rsa && \
-  echo "$SSH_PUBLIC_KEY" > /home/runner/.ssh/id_rsa.pub && \
-  chmod 600 /home/runner/.ssh/id_rsa && \
-  chmod 600 /home/runner/.ssh/id_rsa.pub
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa && \
+  echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_rsa.pub && \
+  chmod 600 ~/.ssh/id_rsa && \
+  chmod 600 ~/.ssh/id_rsa.pub
